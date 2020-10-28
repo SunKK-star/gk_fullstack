f31e18a816bbbc367a04dd3b8332ce2ee1adeb94
SunKK-star <2759812477@qq.com>
2020-10-28 22:21:27 +0800

    数据结构栈

---
+++ stack/lib/Stack.ts
@@ -0,0 +1,31 @@
+export default class Stack {
+  // 属性 java -> js
+  // items 按照一定的方法去操作
+  private items: any[]   // 存储结构
+  constructor() {
+    // 初始化操作
+    this.items = []
+  }
+  push(item:any) {
+    this.items.push(item)
+  }
+  pop() {
+    return this.items.pop()
+  }
+  // 返回栈顶元素
+  peek() {
+    return this.items[this.items.length - 1]
+  }
+  isEmpty() {
+    return this.items.length == 0
+  }
+  clear() {
+    this.items = []
+  }
+  size() {
+    return this.items.length
+  }
+  toString() {
+    return this.items.toString()
+  }
+}
