b93cf015ffccaad52b781e660bcdb6fb55eb427a
SunKK-star <sunkk_star@163.com>
2020-11-30 18:38:22 +0800

    123

---
+++ vue/vue-elemm/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header/Header'
+export default {
+  components: {
+    Header
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/vue-elemm/src/components/Header/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container-wrapper">
+    <div class="avatar">
+      <img width="64" height="64" src="" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
